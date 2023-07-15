0cbe01008672b698c3c24dcb51cd80522bf14671
Willyan <wkinhaamaral@gmail.com>
2023-07-14 22:19:13 -0300

    change routes

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { loadAllProducts, loadSectorProducts, loadSingleProduct } from "../controllers/productsController.js";
 import { cart } from "../controllers/productsController.js";
-import { validateAuth } from "../middlewares/validateAuth.js";
+
 
 
 const productsRouter = Router();
 
 productsRouter.get("/carrinho", cart);
 
-productsRouter.get('/', validateAuth, loadAllProducts);
+productsRouter.get('/', loadAllProducts);
 productsRouter.get('/:sector', loadSectorProducts);
 productsRouter.post('/singleProduct', loadSingleProduct);
 
