27b6da53976679a5e93501cf67a31cd8fe910854
Willyan <wkinhaamaral@gmail.com>
2023-07-14 22:12:18 -0300

    refact: validateauth

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { loadAllProducts, loadSectorProducts } from "../controllers/productsController.js";
 import { cart } from "../controllers/productsController.js";
+import { validateAuth } from "../middlewares/validateAuth.js";
 
 
 const productsRouter = Router();
 
 productsRouter.get("/carrinho", cart);
 
-productsRouter.get('/', loadAllProducts);
+productsRouter.get('/', validateAuth, loadAllProducts);
 productsRouter.get('/:sector', loadSectorProducts);
 
 
